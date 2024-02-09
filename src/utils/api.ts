3923a10a519c0f88cfe7339eade1a942dfa0a37f
andrei.gutan <gutan.andrei@qualitestgroup.com>
2024-02-09 14:48:52 +0200

    add autosave

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,21 @@
 const createURL = (path: string) => window.location.origin + path
 
+export const updateEntry = async (id: string, content: string) => {
+  const res = await fetch(
+    new Request(createURL(`/api/journal/${id}`), {
+      method: 'PATCH',
+      body: JSON.stringify({ content }),
+    }),
+  )
+  if (res.ok) {
+    const data = await res.json()
+    return data.data
+  }
+  if (!res.ok) {
+    console.error(res)
+  }
+}
+
 export const createNewEntry = async () => {
   const res = await fetch(
     new Request(createURL('/api/journal'), {
